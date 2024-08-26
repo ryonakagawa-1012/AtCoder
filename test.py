9cd3d913c5c099053f1e9bbfd83236ccb0d0348e
NAKAGAWA RYO <g2354017@ccmaster.kyoto-su.ac.jp>
2024-08-26 10:50:03 +0900

    Signed-off-by: NAKAGAWA RYO <g2354017@ccmaster.kyoto-su.ac.jp>

--- test.py
+++ test.py
@@ -1,27 +1,5 @@
-from collections import defaultdict
+import sys
 
+n, k = map(int, sys.stdin.readline().split())
 
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-B = []
-
-normal = defaultdict(int)
-answer = 0
-
-loop = sum(A)
-
-total = 0
-for a in A:
-    B.append(total)
-    normal[total % M] += 1
-    total += a
-
-print(B)
-print(normal)
-
-for b in B:
-    normal[b % M] -= 1
-    answer += normal[b % M]
-    normal[(b + loop) % M] += 1
-
-print(answer)
+print(n, k) 

e5e31eababfad4b19d4636df997df4ffbd929fac
Ryo <ryo.na.na.ryo.na@gmail.com>
2024-11-13 19:37:34 +0900

    solve abc379-e

--- ABC/abc379/e/main.py
+++ ABC/abc379/e/main.py
@@ -1,20 +1,27 @@
 import sys
-import time
+# from functools import cache
 
-def input():return sys.stdin.readline().rstrip()
+# sys.setrecursionlimit(10**9)
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+# @cache
+def calc_f(s, i, now_f):
+    if i >= len(s):
+        return 0
+    num = int(s[i])
+    new_f = now_f * 10 + num * (i + 1)
+    return new_f + calc_f(s, i + 1, new_f)
 
 
 def main():
     sys.set_int_max_str_digits(0)
+    sys.set_int_max_str_digits(0)
     n = int(input())
     s = input()
-    ans = 0
-    f = 0
-    for i in range(n):
-        num = int(s[i])
-        f = f*10 + num* (i + 1)
-        ans += f
+    ans = calc_f(s, 0, 0)
     print(ans)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- ABC/abc379/e/random_testcase.py
+++ ABC/abc379/e/random_testcase.py
@@ -19,7 +19,7 @@ from random import randint
 import sys
 sys.set_int_max_str_digits(0)
 
-N = randint(10**5, 2*10**5)
+N = 2*10**5
 S = "9"*N
 
 print(N)
